503c93eb0d2b03d6f51a64d477bd55cd3d82a6b1
maxymats <maksym.mats@gmail.com>
2023-02-27 23:59:30 +0200

    lab1

---
+++ hw1/Program.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hw1
+{
+    public static class Program
+    {
+        static void Main(string[] args)
+        {
+            ToOctalString();
+        }
+        static void ToOctalString()
+        {
+            Console.Write("number = ");
+            int number = int.Parse(Console.ReadLine());
+            string result = string.Empty;
+            int remainder;
+            while (number > 0)
+            {
+                remainder = number % 8;
+                number /= 8;
+                result = remainder.ToString() + result; // додаємо остачу до результату
+            }
+            Console.WriteLine($"{result}");
+
+        }
+        static void CalculationTask()
+        {
+            Console.Write("Введіть x : ");
+            double x = double.Parse(Console.ReadLine());
+            Console.Write("Введіть y : ");
+            double y = double.Parse(Console.ReadLine());
+            Console.Write("Введіть z : ");
+            double z = double.Parse(Console.ReadLine());
+            double a, b, c;
+            a = (x * y) / z + z / (x * y);
+            b = Math.Sin(((x * y) + z) / z) - Math.Cos(1 + (z / (x * y))) + 0.5;
+            c = 2 - Math.Sqrt(Math.Pow(((x * y) / z) + (z / (x * y)), 2) + 5);
+            Console.WriteLine($"a {a}, b {b}, c{c}");
+            double min = a;
+
+            if (b < min)
+            {
+                min = b;
+            }
+
+            if (c < min)
+            {
+                min = c;
+            }
+
+            Console.WriteLine($"Minimal value is {min}");
+        }
+        static void MatrixTask()
+        {
+            int[,] matrixA = new int[5, 7]; 
+            Random rnd = new Random();
+            for (int i = 0; i < 5; i++)
+            {
+                for (int j = 0; j < 7; j++)
+                {
+                    matrixA[i, j] = rnd.Next(100);
+                }
+            }
+
+            Console.WriteLine("Матриця A:");
+            PrintMatrix(matrixA);
+
+            int[] vectorB = new int[5]; // ініціалізуємо вектор розміру 5
+
+            // рахуємо кількість парних елементів у кожному рядку і заповнюємо вектор B
+            for (int i = 0; i < 5; i++)
+            {
+                int count = 0;
+                for (int j = 0; j < 7; j++)
+                {
+                    if (matrixA[i, j] % 2 == 0)
+                    {
+                        count++;
+                    }
+                }
+                vectorB[i] = count;
+            }
+
+            Console.WriteLine("Вектор B:");
+            PrintVector(vectorB); 
+
+            int maxIndex = 0;
+            for (int i = 1; i < 5; i++)
+            {
+                if (vectorB[i] > vectorB[maxIndex])
+                {
+                    maxIndex = i;
+                }
+            }
+
+            Console.WriteLine($"Номер максимального елемента вектора B: {maxIndex}");
+        }
+
+        static void PrintMatrix(int[,] matrix)
+        {
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                {
+                    Console.Write(matrix[i, j] + "\t");
+                }
+                Console.WriteLine();
+            }
+        }
+
+        static void PrintVector(int[] vector)
+        {
+            for (int i = 0; i < vector.Length; i++)
+            {
+                Console.WriteLine(vector[i]);
+            }
+        }
+    }
+}
---
+++ hw1/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("hw1")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("hw1")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("9d02eb2b-2ce7-4af1-ab16-ad54eb550e3b")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
